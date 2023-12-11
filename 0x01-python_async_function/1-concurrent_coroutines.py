076b68e4db87576069ffcf71ba2c70ff8afad72b
Mythamor <bethmithamo.bm@gmail.com>
2023-12-11 23:04:40 +0300

    write an async routine, that takes in 2 int arguments & spawns n times with the specified max_delay

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,13 +6,14 @@ Module: 1-concurrent_coroutines.py
 
 
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     asynchronous routine that takes two integer arguments, n and max_delay
     It spawns wait_random n times with the specified max_delay
     """
-    delays = [await wait_random(max_delay) for _ in range(n)]
+    delays = [float(await wait_random(max_delay)) for _ in range(n)]
     return sorted(delays)
