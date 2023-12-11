8986595a91575a1e9d1ca7309a8ea3b8fcabade7
Mythamor <bethmithamo.bm@gmail.com>
2023-12-11 22:58:48 +0300

    function with integers n, max_delay as args measure the total execution time

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+Module: 2-measure_runtime.py
+"""
+
+
+import time
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    function with integers n and max_delay as arguments that
+    measures the total execution time
+    """
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+
+    total_time = end_time - start_time
+    return total_time / n
