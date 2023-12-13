16ad1b360eecfeac8a50f13a95f15eda5bbacd51
Mythamor <bethmithamo.bm@gmail.com>
2023-12-13 22:21:21 +0300

    coroutine with no args

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Module: 0-async_generator.py
+"""
+
+
+import asyncio
+import random
+
+
+async def async_generator():
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
