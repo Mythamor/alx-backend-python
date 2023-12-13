a534e507887712ef9f5ae8b265a0f2cb3e9208c7
Mythamor <bethmithamo.bm@gmail.com>
2023-12-13 22:34:26 +0300

    coroutine with no args

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,6 +10,11 @@ import random
 
 
 async def async_generator():
+    """
+    coroutine that loops 10X, 
+    each time asynchronously wait 1 second,
+    then yield a random number between 0 and 10
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
