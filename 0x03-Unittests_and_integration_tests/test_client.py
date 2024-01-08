3aa1396efff5b8481e824832e098ff19e7cd493e
Mythamor <bethmithamo.bm@gmail.com>
2024-01-09 02:40:41 +0300

    using patch as a decorator and context manager

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -43,3 +43,39 @@ class TestGithubOrgClient(unittest.TestCase):
                     'repos_url': "https://api.github.com/users/google/repos"}
             self.assertEqual(GithubOrgClient("google")._public_repos_url,
                              "https://api.github.com/users/google/repos")
+
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json: MagicMock) -> None:
+        """
+        unit-test GithubOrgClient.public_repos
+        """
+        test_payload = {'repos': [
+            {
+                "name": "signet",
+                "private": False,
+                "owner": {"login": "googleapi", "id": 16785467},
+            },
+            {
+                "name": "ruby-openid-apps-discovery",
+                "private": False,
+                "owner": {"login": "google", "id": 1342004},
+            },
+        ]}
+
+        mock_get_json.return_value = test_payload['repos']
+
+        org_client = GithubOrgClient("googleapis")
+
+        # Mock the _public_repos_url property
+        with patch("client.GithubOrgClient._public_repos_url",
+                   new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = org_client
+            self.assertEqual(
+                GithubOrgClient("googleapis").public_repos(),
+                [
+                    "signet",
+                    "ruby-openid-apps-discovery",
+                ],
+            )
+            mock_public_repos_url.assert_called_once()
+        mock_get_json.assert_called_once()
